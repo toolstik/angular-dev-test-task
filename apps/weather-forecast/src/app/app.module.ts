9e172652528ee0aef5900fb814b93e95c0d7c3ee
Viktor <tolstik.va@gmail.com>
2021-11-26 01:49:06 +0300

    ngrx packages

--- apps/weather-forecast/src/app/app.module.ts
+++ apps/weather-forecast/src/app/app.module.ts
@@ -1,13 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-	declarations: [AppComponent,],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+
+		EffectsModule.forRoot(),
+		StoreModule.forRoot(
+			{
+				router: routerReducer
+			},
+			{
+				metaReducers: [],
+				runtimeChecks: {
+					strictActionImmutability: true,
+					strictStateImmutability: true,
+				},
+			}
+		),
+		StoreDevtoolsModule.instrument({
+			name: 'Weather App',
+		}),
+		StoreRouterConnectingModule.forRoot(),
+
+		AppRoutingModule
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
